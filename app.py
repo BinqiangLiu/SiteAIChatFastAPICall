c90312161d55aa09bfa6c777140dd9e4ce2168a6
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-11-25 12:59:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,26 +44,27 @@ def query_fastapi(question):
 # 示例查询
 question = st.text_input("Enter your question:")
 if st.button('Get AI Response'):
-    try:
-        ai_response = query_fastapi(question)
-        ai_response_content=ai_response['AIResponse']
-        ai_response_source=ai_response['Sources']
-        #st.write("AI Response:", result.get("AI Response"))
-        
-        st.write("AI Response:", ai_response_content)
-        #st.write("Sources:", ai_response_source)
-        # 使用st.write打印Sources的内容
-        st.write("Sources:")
-        for source in ai_response_source:
-            st.write(source)
-        
-        print("AI Response:", ai_response_content)
-        #print("Sources:", ai_response_source)
-        print("Sources:")
-        for source in ai_response_source:
-            print(source)
-            #ai_response_source中有一个键值source？
-        #Sources: ['https://www.hbsr.com/...cine', '...', 'https://www.hbsr.com/contact']
-        
-    except Exception as e:
-        print(e)
+    with st.spinner('Fetching AI response...'):   
+        try:
+            ai_response = query_fastapi(question)
+            ai_response_content=ai_response['AIResponse']
+            ai_response_source=ai_response['Sources']
+            #st.write("AI Response:", result.get("AI Response"))
+            
+            st.write("AI Response:", ai_response_content)
+            #st.write("Sources:", ai_response_source)
+            # 使用st.write打印Sources的内容
+            st.write("Sources:")
+            for source in ai_response_source:
+                st.write(source)
+            
+            print("AI Response:", ai_response_content)
+            #print("Sources:", ai_response_source)
+            print("Sources:")
+            for source in ai_response_source:
+                print(source)
+                #ai_response_source中有一个键值source？
+            #Sources: ['https://www.hbsr.com/...cine', '...', 'https://www.hbsr.com/contact']
+            
+        except Exception as e:
+            print(e)
